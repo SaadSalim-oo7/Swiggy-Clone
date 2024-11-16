35352c35f52f85b9e6b9400f06f749d74acf1274
Saad Salim <saad.slm25@gmail.com>
2024-11-16 16:05:25 +0530

    first commit

--- src/Components/RestaurantMenu.js
+++ src/Components/RestaurantMenu.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
+
 function RestaurantMenu() {
 
   let params = useParams()
@@ -17,22 +18,49 @@ function RestaurantMenu() {
     })
   }, [])
 
+
   return (
     <div className='container mt-3'>
         <h2>{params.restName}'s Menu.. </h2>
         <div class="accordion mt-4" id="accordionPanelsStayOpenExample">
         { restaurantMenuData.map((item, i) => {
             if (item.card.card.itemCards) {
+                console.log(item.card.card);
+                
             return <div class="accordion-item">
             <h2 class="accordion-header">
-              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={`#panelsStayOpen-collapse${i}`} aria-expanded="true" aria-controls={`panelsStayOpen-collapse${i}`}>
                <b> {item.card.card.title} - {item.card.card.title.length}</b>
               </button>
             </h2>
-            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
+            <div id={`panelsStayOpen-collapse${i}`} class="accordion-collapse collapse show">
               <div class="accordion-body">
-                <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-              </div>
+              { item.card.card.itemCards.map((item, i) => {
+                return  <div class="card mb-3" style={{ width : "max", height : "210px"}}> 
+                <div class="row">
+                <div class="col-md-2 border-0">
+                <img
+                  style={{ height : "210px", width : "320px", borderRadius : "10px"}}
+                  src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_292,h_300,c_fit/${item?.card?.info?.imageId}`}  class="img-fluid rounded-start" alt="..."/>
+                </div>
+                <div class="col-md-8">
+                <div class="card-body">
+                <h5 class="card-title" style={{textAlign : "left"}}><b>{item?.card?.info?.name}</b></h5>
+                <p class="card-text m-0"><i class="bi bi-currency-rupee"></i><b>{ item?.card?.info?.price ? item?.card?.info?.price/100 : item?.card?.info?.defaultPrice/100}</b></p>
+                <p class="card-text m-0"><i class="bi bi-star-fill"></i> {item?.card?.info?.ratings?.aggregatedRating?.rating}</p>
+                <p>{item?.card?.info?.description?.slice(0, 200)}</p>
+                <div className='cont-bottom'>
+                {item?.card?.info?.isVeg ? <p style={{color: 'green'}}><b> Veg </b></p> : <p style={{color : 'red'}}><b> Non-Veg</b></p>}
+                <button type="button" class="btn btn-success">ADD</button>
+                </div>
+                
+        </div>
+      </div>
+    </div>
+    </div>
+              })}
+    </div>
+             
             </div>
           </div>
             }
@@ -44,4 +72,4 @@ function RestaurantMenu() {
   )
 }
 
-export default RestaurantMenu
+export default RestaurantMenu;
